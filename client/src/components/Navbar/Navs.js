1ec07b4a3f289a2b23ffeaaa0367889f27c418da
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-02-19 14:41:36 -0800

    added custom navbar labels relevant to gtr

--- client/src/components/Navbar/Navs.js
+++ client/src/components/Navbar/Navs.js
@@ -6,16 +6,29 @@ class Navs extends Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#features">About</Nav.Link>
+            <NavDropdown
+              title="Product & Services"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Catalog</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Upgrade Packages
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Accomplishments" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">GTR Press</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Customer Photos
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
@@ -25,9 +38,9 @@ class Navs extends Component {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link href="#deets">Events</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+              Contact
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
