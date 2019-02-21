f33bba6578937bcf41b71090e781cb945333c3e2
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-02-21 15:45:04 -0800

    added bottom history doc background with it set to height fully

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Container, Col, Row, Figure } from "react-bootstrap";
 import Gonz from "../../components/Gonz/Gonz.js";
+import historyBot from "../../images/historyBot.png";
 import "./about.css";
 
 class About extends Component {
@@ -31,7 +32,7 @@ class About extends Component {
                 </ul>
               </section>
             </Col>
-            <Col lg={6}>
+            <Col id="historyOne" lg={6}>
               <header>
                 <h3>The History of GTR</h3>
               </header>
@@ -92,7 +93,16 @@ class About extends Component {
                 </ul>
               </section>
             </Col>
-            <Col lg={6}>
+            <Col
+              style={{
+                backgroundImage: `url(${historyBot})`,
+
+                backgroundPosition: "center",
+                backgroundRepeat: "no - repeat",
+                backgroundSize: "cover"
+              }}
+              lg={6}
+            >
               <section>
                 <article className="justify">
                   Originating from a modest 1000 sq./ft. shop with one bay for
@@ -118,93 +128,9 @@ class About extends Component {
           </Row>
           {/* Third Row */}
           <Row>
-            <Col lg={6}>
-              <header>
-                <h3>The History of GTR</h3>
-              </header>
-              <section>
-                <article className="justify">
-                  Founded in 1997, GTR High Performance was created by two
-                  long-time Mustang enthusiasts, Gonzalo & Ricardo Topete. The
-                  concept was simple: to create a positive and rewarding Mustang
-                  shop experience. With a courteous, professional, and
-                  knowledgeable staff, GTR High Performance has emerged as one
-                  of the premier Mustang performance shops in the country.
-                  Regular appearances and features in national Mustang
-                  publications such as: 5.0 Mustangs & SuperFords Magazine,
-                  Mustang Enthusiast Magazine and media such as PowerTV are a
-                  testament to the quality of work available at GTR High
-                  Performance.
-                </article>
-              </section>
-              {/* <article className="justify">
-                  Originating from a modest 1000 sq./ft. shop with one bay for
-                  installations and a tiny office, GTR High Performance forged a
-                  strong reputation for quality workmanship, excellent customer
-                  service, and competetive pricing. Over the course of a decade,
-                  GTR High Performance expanded its services by offering a
-                  larger line of top-quality performance parts and accessories
-                  for Mustangs. The increased growth created the need for a
-                  larger facility. GTR High Performance, now in its 3rd
-                  location, offers the Mustang enthusiast the ultimate toy
-                  store. Comprised of over 8500 sq./ft. of Mustang paradise
-                  featuring a fully stocked parts warehouse, showroom, customer
-                  waiting room and state-of-the-art installation center. The
-                  highlight of GTR's installation shop is a DynoJet 224X chassis
-                  dynomometer. Capable of registering up to 1500 horsepower and
-                  reaching speeds up to 200 mph, it can tame even the wildest
-                  Pony! Contact GTR High Performance to schedule your dyno test
-                  or tuning session.
-                </article>
-                <section>
-                  <aside className="justify">
-                    Next time you are in the market for Mustang performance
-                    parts and accessories or installation help or simply a
-                    dyno-test, contact GTR High Performance. You and your
-                    Mustang will be treated with the respect and attention to
-                    detail you deserve and expect. Remember, whether you plan on
-                    building a Mustang for the drag strip, road-course,
-                    show-circuit, or simply fun on the street, GTR High
-                    Performance IS your Mustang Race Place!
-                  </aside>
-                </section> */}
-            </Col>
+            <Col lg={6} />
           </Row>
         </Container>
-        {/* <header>
-          <h3>The History of GTR</h3>
-        </header>
-        <section>
-          <p>
-            Founded in 1997, GTR High Performance was created by two long-time
-            Mustang enthusiasts, Gonzalo & Ricardo Topete. The concept was
-            simple: to create a positive and rewarding Mustang shop experience.
-            With a courteous, professional, and knowledgeable staff, GTR High
-            Performance has emerged as one of the premier Mustang performance
-            shops in the country. Regular appearances and features in national
-            Mustang publications such as: 5.0 Mustangs & SuperFords Magazine,
-            Mustang Enthusiast Magazine and media such as PowerTV are a
-            testament to the quality of work available at GTR High Performance.
-          </p>
-          <p>
-            Originating from a modest 1000 sq./ft. shop with one bay for
-            installations and a tiny office, GTR High Performance forged a
-            strong reputation for quality workmanship, excellent customer
-            service, and competetive pricing. Over the course of a decade, GTR
-            High Performance expanded its services by offering a larger line of
-            top-quality performance parts and accessories for Mustangs. The
-            increased growth created the need for a larger facility. GTR High
-            Performance, now in its 3rd location, offers the Mustang enthusiast
-            the ultimate toy store. Comprised of over 8500 sq./ft. of Mustang
-            paradise featuring a fully stocked parts warehouse, showroom,
-            customer waiting room and state-of-the-art installation center. The
-            highlight of GTR's installation shop is a DynoJet 224X chassis
-            dynomometer. Capable of registering up to 1500 horsepower and
-            reaching speeds up to 200 mph, it can tame even the wildest Pony!
-            Contact GTR High Performance to schedule your dyno test or tuning
-            session.
-          </p>
-        </section> */}
         {/* <aside>
           Next time you are in the market for Mustang performance parts and
           accessories or installation help or simply a dyno-test, contact GTR
